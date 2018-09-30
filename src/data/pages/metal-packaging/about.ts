b705f8aaf1b97d06b4f415db920a858007c669eb
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-09-30 15:35:41 +0800

    add baple-metal-packaing

--- src/data/pages/metal-packaging/about.ts
+++ src/data/pages/metal-packaging/about.ts
@@ -5,7 +5,7 @@ export const frontmatter: AboutPage = {
   sections: {
     about1: {
       logo: "../../img/logos/baple-plastics-logo.png",
-      image: "../../img/plastics/1_Products_Welcome.jpg",
+      image: "../../img/metal-packaging/Aerosol Can.jpg",
       heading: "Baple Metal Packaging",
       body: [
         "Baple fue fundada en 2013 con una clara visión y misión.",
@@ -14,15 +14,15 @@ export const frontmatter: AboutPage = {
       ],
     },
     about2: {
-      image: "../../img/plastics/2WelcomeImage_Bombalocion.jpg",
+      image: "../../img/metal-packaging/Metal Packaging_Valves.jpg",
       body: [
-        "En la actualidad, Baple Plastics ofrece unos de los más amplios catálogos del mercado nacional gracias a la experiencia acumulada desde nuestro comienzo en el año 2013, obeteniendo un incesante crecimiento anual año tras año.",
+        "En la actualidad, Baple Metal Packaging ofrece unos de los más amplios catálogos del mercado nacional gracias a la experiencia acumulada desde nuestro comienzo en el año 2013, obeteniendo un incesante crecimiento anual año tras año.",
         "Nuestros partners disfrutan de las ventajas de Baple Plastics: múltiples centros de fabricacion, que aportan flexibilidad, amplio rango de productos y soluciones competitivas.",
         "Innovación, Tecnología, y Servicio Personalizado son los pilares fundamentales de Baple para crear una cooperación fuerte, sostenible y durareda con sus partners. Siendo este el valor añadido que sólo un proveedor especializado es capaz de aportar.",
       ],
     },
     advantages: {
-      heading: "Advantages of Baple Plastics",
+      heading: "Advantages of Baple Metal Packaging",
       body: [
         "Innovación y Tecnología es la base para poder suministrar productos con los niveles de calidad exigidos por el mercado internacional, ofreciendo soluciones cada vez más modernas y más personalizadas a nuestros partners.",
       "Debido a los múltiples centros de fabricación, tenemos la capacidad de ofrecer una gran flexibilidad en la personalización de productos y servicios adicionales que aporten valor añadido al consumidor.",
