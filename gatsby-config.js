1eece82a7da8ee1257c8bbd31306ec8fc284ede4
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-10-05 22:09:16 +0800

    install plugin favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,15 +5,21 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/images/logos/baple-group-logo.png',
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'baple-group',
+        short_name: 'baple',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: './src/images/logos/baple-group-logo.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
